4d44048eb31e9684d50dffc7b1c96e33d32ca0cb
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-17 16:18:08 +0200

    Configure gulp.js

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+// The default Gulp.js task
+gulp.task('default', ['js', 'sass', 'watch']);
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task("sass", function () {
+    return gulp.src('assets/scss/*.scss')
+            // .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(cleanCSS({
+                compatibility: 'ie11'
+            }))
+            .pipe(autoprefixer({
+                browsers: [
+                    "last 5 versions",
+                    "ie >= 11"
+                ]
+            }))
+            // .pipe(sourcemaps.write())
+            .pipe(gulp.dest('assets/css'));
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task("js", function () {
+    return gulp
+            .src([
+                "node_modules/bootstrap/dist/js/bootstrap.min.js",
+                "node_modules/jquery/dist/jquery.min.js",
+                "node_modules/popper.js/dist/umd/popper.min.js"
+            ])
+            .pipe(gulp.dest("assets/js"));
+});
+
+// Watch for LESS and JS file changes
+gulp.task("watch", function () {
+    gulp.watch(["assets/scss/**/*.scss"], ["sass"]);
+    gulp.watch(['assets/js/**/*.js'], ['js']);
+});
