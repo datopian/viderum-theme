26843113b083d3e286521bf3667b70fc5f6f687c
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-23 16:53:25 +0200

    Add .site-footer content

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
 </div><!-- .container -->
+<footer class="site-footer bg-dark">
+    <div class="container d-flex flex-wrap justify-content-between">
+        <?php get_template_part( '/snippets/sidebars/sidebar', 'footer' ); ?>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 <?php get_template_part( '/snippets/footer/google-analytics' ); ?>
 </body>
