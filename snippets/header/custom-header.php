01a66c3e92f401465789d0bd3a6ccb4c40c2471d
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-23 13:41:56 +0200

    Implement Hero section with title and description

--- snippets/header/custom-header.php
+++ snippets/header/custom-header.php
@@ -1,6 +1,27 @@
+<?php
+
+$theme_settings = get_theme_settings();
+
+?>
 <div class="custom-header">
 
     <div class="custom-header-media">
+        <div class="container-fluid">
+            <div class="row">
+                <header class="offset-md-1 col-md-5 page-header">
+                    <?php if ( is_front_page() ): ?>
+                        <?php if ( isset( $theme_settings[ 'hero_title' ] ) ): ?>
+                            <h1 class="page-title"><?php echo esc_html( $theme_settings[ 'hero_title' ] ); ?></h1>
+                        <?php endif; ?>
+                        <?php if ( isset( $theme_settings[ 'hero_description' ] ) ): ?>
+                            <p class="lead"><?php echo esc_html( $theme_settings[ 'hero_description' ] ); ?></p>
+                        <?php endif; ?>
+                    <?php else: ?>
+                        <h1 class="page-title"><?php single_post_title(); ?></h1>
+                    <?php endif; ?>
+                </header>
+            </div>
+        </div>
         <?php the_custom_header_markup(); ?>
     </div>
 
