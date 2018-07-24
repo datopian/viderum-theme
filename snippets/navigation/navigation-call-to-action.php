377560c0cc2b02c23dee2b412450d4df45653f39
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-24 16:24:44 +0200

    Add Call to Action navigation

---
+++ snippets/navigation/navigation-call-to-action.php
@@ -0,0 +1,8 @@
+<?php
+
+wp_nav_menu (array(
+    'theme_location' => 'call_to_action',
+    'container_id' => 'callToActionNavContent',
+    'container_class' => 'd-flex',
+    'menu_class' => 'menu call-to-action-menu nav'
+) );
