2f9d444fa24fdbf33e8dc93b41d32d891e6fe647
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-26 17:01:18 +0200

    Complete generic index.php template

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
-<?php get_header(); ?>
+<?php
 
-<?php get_footer(); ?>
+get_header();
+
+if ( have_posts() ):
+
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <?php
+
+                while ( have_posts() ):
+
+                    the_post();
+
+                    if ( is_page() ):
+                        get_template_part( '/snippets/page/content', 'page' );
+                    else:
+                        get_template_part( '/snippets/post/content' );
+                    endif;
+
+
+                endwhile;
+
+                ?>
+            </div>
+            <div class="col-md-4 offset-md-1">
+                <?php
+
+                if ( is_page() ):
+                    get_template_part( '/snippets/sidebars/sidebar', 'page' );
+                else:
+                    get_template_part( 'sidebar');
+                endif;
+
+                ?>
+            </div>
+        </div>
+    </div>
+    <?php
+
+endif;
+
+get_footer();
