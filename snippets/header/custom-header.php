a376a7b74649fd85df2b0d4ad379066ad9b02e47
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-27 16:44:51 +0200

    Refactor custom-header.php

--- snippets/header/custom-header.php
+++ snippets/header/custom-header.php
@@ -2,11 +2,15 @@
 
 $theme_settings = get_theme_settings();
 $container_class = 'container-fluid';
-$grid_class = 'offset-md-1 col-md-5';
+$grid_class = 'col-lg-8 offset-lg-1';
+$page_header_class = '';
 
 if ( !is_front_page() ):
-    $container_class = 'container';
-    $grid_class = 'col-md-8';
+    $grid_class = 'col-lg-8 offset-lg-2';
+endif;
+
+if ( is_single() || is_author() ):
+    $page_header_class = 'page-header-avatar';
 endif;
 
 ?>
@@ -14,44 +18,55 @@ endif;
 
     <div class="custom-header-media">
         <div class="<?php echo esc_attr( $container_class ); ?>">
-            <div class="row">
-                <header class="<?php echo esc_attr( $grid_class ); ?> page-header">
-                    <?php if ( is_front_page() ): ?>
-                        <?php if ( isset( $theme_settings[ 'hero_title' ] ) ): ?>
-                            <h1 class="page-title"><?php echo esc_html( $theme_settings[ 'hero_title' ] ); ?></h1>
-                        <?php endif; ?>
-                        <?php if ( isset( $theme_settings[ 'hero_description' ] ) ): ?>
-                            <p class="lead"><?php echo esc_html( $theme_settings[ 'hero_description' ] ); ?></p>
-                        <?php endif; ?>
-                        <?php if ( isset( $theme_settings[ 'hero_link' ] ) ): ?>
-                            <a href="<?php echo the_permalink( $theme_settings[ 'hero_link' ] ) ?>" class="btn btn-lg btn-primary btn-hero"><?php echo __( 'More about', 'viderum' ) . ' ' . get_the_title( $theme_settings[ 'hero_link' ] ); ?></a>
-                        <?php endif; ?>    
-                    <?php elseif ( is_archive() ): ?>
-                        <h1 class="page-title"><?php _e('Archive', 'viderum'); ?></h1>
-                    <?php elseif ( is_post_type_archive() ): ?>
-                        <h1 class="page-title"><?php post_type_archive_title(); ?></h1>
-                    <?php else: ?>
-                        <h1 class="page-title"><?php single_post_title(); ?></h1>
-                    <?php endif; ?>
-                </header>
-                <?php
-
-                if ( is_page() ):
-                    if ( has_post_thumbnail() ):
-
-                        ?>
-                        <div class="col-md-4 d-none d-md-block">
-                            <figure class="custom-header-icon">
-                                <?php the_post_thumbnail(); ?>
-                            </figure>
-                        </div>
-
+            <div class="<?php echo esc_attr( $grid_class ); ?>">
+                <div class="row">
+                    <header class="page-header col-lg-8 <?php echo esc_attr( $page_header_class ); ?>">
                         <?php
 
-                    endif;
-                endif;
+                        if ( is_single() || is_author() ):
+                            viderum_author_avatar( get_post_field( 'post_author', get_the_ID() ) );
+                        endif;
 
-                ?>
+                        ?>
+                        <?php if ( is_front_page() ): ?>
+                            <?php if ( isset( $theme_settings[ 'hero_title' ] ) ): ?>
+                                <h1 class="page-title"><?php echo esc_html( $theme_settings[ 'hero_title' ] ); ?></h1>
+                            <?php endif; ?>
+                            <?php if ( isset( $theme_settings[ 'hero_description' ] ) ): ?>
+                                <p class="lead"><?php echo esc_html( $theme_settings[ 'hero_description' ] ); ?></p>
+                            <?php endif; ?>
+                            <?php if ( isset( $theme_settings[ 'hero_link' ] ) ): ?>
+                                <a href="<?php echo the_permalink( $theme_settings[ 'hero_link' ] ) ?>" class="btn btn-lg btn-primary btn-hero"><?php echo __( 'More about', 'viderum' ) . ' ' . get_the_title( $theme_settings[ 'hero_link' ] ); ?></a>
+                            <?php endif; ?>    
+                        <?php elseif ( is_archive() ): ?>
+                            <h1 class="page-title"><?php _e( 'Archive', 'viderum' ); ?></h1>
+                        <?php elseif ( is_search() ): ?>
+                            <h1 class="page-title"><?php _e( 'Search', 'viderum' ); ?> : <span class="text-secondary"><?php echo get_search_query(); ?></span></h1>
+                        <?php elseif ( is_post_type_archive() ): ?>
+                            <h1 class="page-title"><?php post_type_archive_title(); ?></h1>
+                        <?php else: ?>
+                            <h1 class="page-title"><?php single_post_title(); ?></h1>
+                        <?php endif; ?>
+                    </header>
+                    <?php
+
+                    if ( is_page() ):
+                        if ( has_post_thumbnail() ):
+
+                            ?>
+                            <div class="col-lg-4 d-none d-md-block">
+                                <figure class="custom-header-icon">
+                                    <?php the_post_thumbnail(); ?>
+                                </figure>
+                            </div>
+
+                            <?php
+
+                        endif;
+                    endif;
+
+                    ?>
+                </div>
             </div>
         </div>
         <?php
