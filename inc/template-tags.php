df61e0e8eb08e1332af029c729655069aed50ae5
Goce Mitevski <goce.mitevski@keitaro.com>
2018-08-01 12:52:00 +0200

    Remove updated time from viderum_posted_on()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -20,7 +20,7 @@ function viderum_posted_on() {
 function viderum_time_link() {
     $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
     if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
     }
 
     $time_string = sprintf( $time_string, get_the_date( DATE_W3C ), get_the_date(), get_the_modified_date( DATE_W3C ), get_the_modified_date()
