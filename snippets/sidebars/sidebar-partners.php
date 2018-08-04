9854b6d9dbacdf304f959e332d7b5d3abf9acea5
Goce Mitevski <goce.mitevski@keitaro.com>
2018-08-04 16:46:13 +0200

    Extend Partners wrapper

--- snippets/sidebars/sidebar-partners.php
+++ snippets/sidebars/sidebar-partners.php
@@ -7,16 +7,12 @@ if ( is_active_sidebar( $sidebar_id ) ) :
     ?>
 
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-10 offset-md-1">
-                <section class="partners">
-                    <h2 class="title"><?php _e( 'Our Partners' ); ?></h2>
-                    <div class="list">
-                        <?php dynamic_sidebar( $sidebar_id ); ?>
-                    </div>
-                </section>
+        <section class="partners">
+            <h2 class="title"><?php _e( 'Our Partners' ); ?></h2>
+            <div class="list">
+                <?php dynamic_sidebar( $sidebar_id ); ?>
             </div>
-        </div>
+        </section>
     </div>
 
     <?php
