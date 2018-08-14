160faa127152a4c05fbc6c39be48c172a05968cc
Goce Mitevski <goce.mitevski@keitaro.com>
2018-08-14 12:38:04 +0200

    Remove text-center CSS class

--- snippets/post/content-card.php
+++ snippets/post/content-card.php
@@ -3,7 +3,7 @@
         <?php if ( has_post_thumbnail() ): ?>
             <a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><img class="card-img-top" src="<?php echo esc_attr( the_post_thumbnail_url() ); ?>" alt="<?php the_title(); ?>"></a>
         <?php endif; ?>
-        <div class="card-body text-center">
+        <div class="card-body">
             <?php the_title( '<h2 class="card-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
             <p class="card-text">
                 <?php echo get_the_excerpt(); ?>
